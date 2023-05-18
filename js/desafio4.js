05ca8d5a066b0d4cc0947c5a4bfb2e0729ed85b4
Damian Paz <damjpv@gmail.com>
2023-05-18 12:56:12 -0300

    corrijo error

--- js/desafio4.js
+++
@@ -1,8 +0,0 @@
-
-
-var tempF= prompt("Ingrese la temperatura en grados Fahrenheit:");
-tempF=Number(tempF);
-
-var tempC=(tempF-32)/1.8;
-
-alert(`La temperatura en grados Celsius es: ${Math.round(tempC)}`);
